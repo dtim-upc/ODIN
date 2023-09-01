cbc744f02608b93b0e591aa532ed0e5c08dcbf00
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:17:34 +0200

    Update AppConfig.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/config/AppConfig.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/config/AppConfig.java
@@ -4,20 +4,37 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class to manage application properties.
+ */
 @Getter
 @Configuration
 public class AppConfig {
+
+    /**
+     * Constructor for the AppConfig class.
+     * The constructor implementation is empty, as no additional logic is required at the moment.
+     */
     public AppConfig() {
         // Constructor implementation is empty.
         // No additional logic is required at the moment.
     }
 
+    /**
+     * Database type property retrieved from application.properties.
+     */
     @Value("${dataStorage.DataBaseType}")
     public String DBTypeProperty;
 
+    /**
+     * Disk path property retrieved from application.properties.
+     */
     @Value("${dataStorage.diskPath}")
     public String diskPath;
 
+    /**
+     * Jena path property retrieved from application.properties.
+     */
     @Value("${dataStorage.JenaPath}")
     private String JenaPath;
 }
