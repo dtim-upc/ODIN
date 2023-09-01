529d54e731229f02e7a4ae69e772b8023801808d
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:39:56 +0200

    Update Links.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/graphy/Links.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/graphy/Links.java
@@ -2,15 +2,34 @@ package edu.upc.essi.dtim.odin.NextiaGraphy.graphy;
 
 import lombok.Data;
 
+/**
+ * Represents a link in a graph connecting two nodes.
+ */
 @Data
 public class Links {
 
-    String id;
-    String nodeId;
-    String source;
-    String target;
-    String label;
+    /**
+     * The unique identifier of the link.
+     */
+    private String id;
 
+    /**
+     * The identifier of the node associated with this link.
+     */
+    private String nodeId;
 
+    /**
+     * The source node of the link.
+     */
+    private String source;
 
+    /**
+     * The target node of the link.
+     */
+    private String target;
+
+    /**
+     * The label or description of the link.
+     */
+    private String label;
 }
