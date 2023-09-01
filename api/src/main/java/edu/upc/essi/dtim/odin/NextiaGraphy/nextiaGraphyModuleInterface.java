9f6ff532f50bcaca78dcdd1642e561fb20e09481
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:52:38 +0200

    Update nextiaGraphyModuleInterface.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/nextiaGraphyModuleInterface.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/nextiaGraphyModuleInterface.java
@@ -3,5 +3,11 @@ package edu.upc.essi.dtim.odin.NextiaGraphy;
 import edu.upc.essi.dtim.NextiaCore.graph.Graph;
 
 public interface nextiaGraphyModuleInterface {
+    /**
+     * Generates a visual graph representation from an RDF graph.
+     *
+     * @param graph The RDF graph to generate the visual graph from.
+     * @return A JSON representation of the visual graph.
+     */
     String generateVisualGraph(Graph graph);
 }
