2569fd10fad4b2aaaa05e3379ac38c4620b52e29
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:38:16 +0200

    Update bsModuleInterface.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleInterface.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleInterface.java
@@ -5,5 +5,11 @@ import edu.upc.essi.dtim.NextiaCore.datasources.dataset.Dataset;
 import edu.upc.essi.dtim.NextiaCore.graph.Graph;
 
 public interface bsModuleInterface {
+    /**
+     * Converts a dataset represented as a DataResource into a Graph.
+     *
+     * @param dataset The dataset to be converted into a graph.
+     * @return The resulting graph representing the dataset.
+     */
     Graph convertDatasetToGraph(DataResource dataset);
 }
