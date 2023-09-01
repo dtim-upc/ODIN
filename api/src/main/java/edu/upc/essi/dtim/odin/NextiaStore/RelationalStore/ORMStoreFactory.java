8b0c84861dce1374d46d70127c55e7c3dc952fcc
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:56:07 +0200

    Update ORMStoreFactory.java

--- api/src/main/java/edu/upc/essi/dtim/odin/NextiaStore/RelationalStore/ORMStoreFactory.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/NextiaStore/RelationalStore/ORMStoreFactory.java
@@ -4,19 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * Factory class responsible for creating instances of {@link ORMStoreInterface}.
+ * It ensures that only one instance of the ORM implementation is created and returned
+ * for the lifetime of the application.
+ */
 @Component
 public class ORMStoreFactory {
-    private static final Logger logger = LoggerFactory.getLogger(ORMStoreFactory.class);
 
+    private static final Logger logger = LoggerFactory.getLogger(ORMStoreFactory.class);
     private static ORMStoreInterface ormStoreInterfaceInstance = null;
 
+    /**
+     * Private constructor prevents instantiation from outside the class.
+     */
     private ORMStoreFactory() {
         // Private constructor to prevent instantiation from outside the class
     }
 
+    /**
+     * Get an instance of {@link ORMStoreInterface}.
+     * If an instance does not exist, a new instance of the default ORM implementation {@link OrmStoreJpaImpl} is created.
+     *
+     * @return An instance of {@link ORMStoreInterface}.
+     */
     public static ORMStoreInterface getInstance() {
         if (ormStoreInterfaceInstance == null) {
-            logger.info("Creating new instance of JpaOrmImplementation");
+            logger.info("Creating a new instance of JpaOrmImplementation");
             ormStoreInterfaceInstance = new OrmStoreJpaImpl();
         }
         return ormStoreInterfaceInstance;
