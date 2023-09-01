8d7b36762356030c67228a180ad840ad59a7f768
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:45:25 +0200

    Update Nodes.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/graphy/Nodes.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/NextiaGraphy/graphy/Nodes.java
@@ -6,99 +6,88 @@ import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.XSD;
 
-
+/**
+ * Represents nodes in a graph used in NextiaGraphy.
+ */
 @Data
 public class Nodes {
+    String id;          // Unique identifier for the node
+    String iri;         // IRI (Internationalized Resource Identifier) of the node
+    String iriType;     // Type of the IRI
+    String shortType;   // Shortened type of the IRI
+    String type;        // Type of the node (class, object, or datatype)
+    String label;       // Label associated with the node
+    String domain;      // Domain of the node (for properties)
+    String range;       // Range of the node (for properties)
+    Boolean isIntegrated; // Indicates if the node is integrated (true/false)
 
-    String id;
-    String iri;
-    String iriType;
-    String shortType;
-    String type;
-    String label;
-    String domain;
-    String range;
-    Boolean isIntegrated;
+    // Optional
+    String linkId;      // Identifier of a linked node (optional)
 
-    // optional
-    String linkId;
-
-    public Nodes(){
+    /**
+     * Default constructor for Nodes.
+     */
+    public Nodes() {
         this.domain = "";
         this.range = "";
         this.isIntegrated = false;
     }
 
-    public void setXSDDatatype(){
+    /**
+     * Sets the node type to "xsdType" and the short type to "xsd:String".
+     */
+    public void setXSDDatatype() {
         this.type = "xsdType";
         this.shortType = "xsd:String";
     }
 
-    public void computeShortType(){
-//        this if it's because rdfs:subclass does not have a type...
-        if(iriType != null) {
-            if(iriType.contains(RDFS.getURI())){
-                this.shortType = "rdfs:"+ iriType.replace(RDFS.getURI(),"");
+    /**
+     * Computes the short type based on the IRI type.
+     */
+    public void computeShortType() {
+        // Check if the IRI type is not null
+        // this if it's because rdfs:subclass does not have a type...
+        if (iriType != null) {
+            if (iriType.contains(RDFS.getURI())) {
+                this.shortType = "rdfs:" + iriType.replace(RDFS.getURI(), "");
             } else if (iriType.contains(RDF.getURI())) {
-                this.shortType = "rdf:"+ iriType.replace(RDF.getURI(),"");
+                this.shortType = "rdf:" + iriType.replace(RDF.getURI(), "");
             } else if (iriType.contains("http://www.essi.upc.edu/DTIM/NextiaDI/")) {
-                this.shortType = "nextia:" + iriType.replace("http://www.essi.upc.edu/DTIM/NextiaDI/","");
+                this.shortType = "nextia:" + iriType.replace("http://www.essi.upc.edu/DTIM/NextiaDI/", "");
             } else {
                 this.shortType = iriType;
             }
         }
-
-//        this.shortType = ""
     }
 
+    /**
+     * Computes the type of the node based on its range and IRI type.
+     */
     public void computeType() {
-
-        // think better way to do this
-
-//        if(iriType.equals(RDFS.Class.getURI())){
-//            type = "class";
-//        }
-        if(!range.equals("") )     { // everything that contains range is property
-            if( range.contains(XSD.getURI())) {
-//                type = "datatypeProperty";
-
-//                TODO: set in Nextiadi a variable that can be used in external
+        if (!range.equals("")) { // Everything that contains a range is a property
+            if (range.contains(XSD.getURI())) {
                 type = "datatype";
-                if(iriType != null)
+                if (iriType != null) {
                     if (iriType.equals(Vocabulary.IntegrationDProperty.val())) {
                         isIntegrated = true;
-//                                "integratedDatatypeProperty";
                     }
-
-
-//            } else if ( iriType.equals(RDF.Property.getURI())  ) {
-//                type = "objectProperty";
+                }
             } else {
                 type = "object";
-                if(iriType != null)
+                if (iriType != null) {
                     if (iriType.equals(Vocabulary.IntegrationOProperty.val())) {
                         isIntegrated = true;
-//                                "integratedObjectProperty";
                     }
-
-//                type = "objectProperty"; // missing subclassof and subpropertyof
-
+                }
             }
         } else {
             type = "class";
-            if(iriType != null) {
+            if (iriType != null) {
                 if (iriType.equals(Vocabulary.IntegrationClass.val())) {
-//                    type = "integratedClass";
                     isIntegrated = true;
                 }
             }
-
-
-
         }
         computeShortType();
-
-
     }
-
 }
