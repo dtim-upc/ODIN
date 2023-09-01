25572add6b6aecd4def9d7d765fe54b7fd2e9599
Víctor Asenjo <victorasenj@gmail.com>
2023-09-01 13:18:43 +0200

    Update CorsConfiguration.java #16

--- api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
@@ -5,14 +5,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for Cross-Origin Resource Sharing (CORS).
+ */
 @Configuration
 @EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer
-{
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Configure CORS mappings.
+     *
+     * @param registry The CorsRegistry to configure.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST","PUT","DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowedOrigins("http://localhost:8080/", "http://localhost:9000/")
                 .maxAge(3600);
