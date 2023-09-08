eb3e7e4c618899aa63f778fca667d28bed3206cb
Víctor Asenjo <victorasenj@gmail.com>
2023-09-08 12:04:12 +0200

    Update FormNewDataSource.vue

--- frontend/src/components/forms/FormNewDataSource.vue
+++ frontend/src/components/forms/FormNewDataSource.vue
@@ -394,9 +394,12 @@ const successCallback = (datasource) => {
 const triggerFileUpload = () => {
   const fileUploadInput = document.createElement('input');
   fileUploadInput.type = 'file';
+  fileUploadInput.accept = '.csv, .json'; // Specify accepted file extensions
+  fileUploadInput.multiple = true; // Enable multiple file selection
   fileUploadInput.addEventListener('change', handleFileUpload);
   fileUploadInput.click();
 };
+
 
 // Método para abrir el selector de carpetas
 const triggerFolderUpload = () => {
