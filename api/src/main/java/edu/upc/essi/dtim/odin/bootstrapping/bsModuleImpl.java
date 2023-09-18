2353dc1718641f61c69e6b4dcadb9e297248a930
Víctor Asenjo <victorasenj@gmail.com>
2023-09-18 14:29:04 +0200

    versions bs

--- api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleImpl.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleImpl.java
@@ -11,8 +11,8 @@ import org.apache.jena.rdf.model.ModelFactory;
 
 import edu.upc.essi.dtim.nextiabs2.CSVBootstrap_with_DataFrame_MM_without_Jena;
 import edu.upc.essi.dtim.nextiabs2.JSONBootstrap_with_DataFrame_MM_without_Jena;
-//import edu.upc.essi.dtim.nextiadi.bootstraping.CSVBootstrap;
-//import edu.upc.essi.dtim.nextiadi.bootstraping.JSONBootstrapSWJ;
+import edu.upc.essi.dtim.nextiadi.bootstraping.CSVBootstrap;
+import edu.upc.essi.dtim.nextiadi.bootstraping.JSONBootstrapSWJ;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,30 +30,32 @@ public class bsModuleImpl implements bsModuleInterface{
      */
     @Override
     public Graph convertDatasetToGraph(DataResource dataset) {
+        int bsVersion = 1;
+
         Graph bootstrapG = CoreGraphFactory.createGraphInstance("normal");
 
-        // Bloque de código deprecado
-        //Model bootstrapM = convertDatasetToModel(dataset);
-        //bootstrapG.setGraph(bootstrapM);
-
-        /* TODO: update when new BS is ready*/
-        if (dataset.getClass().equals(CsvDataset.class)) {
-            CSVBootstrap_with_DataFrame_MM_without_Jena bootstrap = new CSVBootstrap_with_DataFrame_MM_without_Jena(dataset.getId(), ((CsvDataset) dataset).getDatasetName(), ((CsvDataset) dataset).getPath());
-            try {
-                bootstrapG = bootstrap.bootstrapSchema();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        if(bsVersion == 0) {
+            // Bloque de código deprecado
+            Model bootstrapM = convertDatasetToModel(dataset);
+            bootstrapG.setGraph(bootstrapM);
+        } else if (bsVersion == 1) {
+            /* TODO: update when new BS is ready*/
+            if (dataset.getClass().equals(CsvDataset.class)) {
+                CSVBootstrap_with_DataFrame_MM_without_Jena bootstrap = new CSVBootstrap_with_DataFrame_MM_without_Jena(dataset.getId(), ((CsvDataset) dataset).getDatasetName(), ((CsvDataset) dataset).getPath());
+                try {
+                    bootstrapG = bootstrap.bootstrapSchema();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (dataset.getClass().equals(JsonDataset.class)) {
+                JSONBootstrap_with_DataFrame_MM_without_Jena j = new JSONBootstrap_with_DataFrame_MM_without_Jena(dataset.getId(), ((JsonDataset) dataset).getDatasetName(), ((JsonDataset) dataset).getPath());
+                try {
+                    bootstrapG = j.bootstrapSchema();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        } else if (dataset.getClass().equals(JsonDataset.class)) {
-            JSONBootstrap_with_DataFrame_MM_without_Jena j = new JSONBootstrap_with_DataFrame_MM_without_Jena(dataset.getId(), ((JsonDataset) dataset).getDatasetName(), ((JsonDataset) dataset).getPath());
-            try {
-                bootstrapG = j.bootstrapSchema();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
+        } else{}
 
         return bootstrapG;
     }
@@ -64,7 +66,6 @@ public class bsModuleImpl implements bsModuleInterface{
      * @param dataset El conjunto de datos que se va a convertir.
      * @return Un modelo RDF que representa el conjunto de datos.
      */
-    /*
     Model convertDatasetToModel(DataResource dataset) {
         Model bootstrapM = ModelFactory.createDefaultModel();
         if (dataset.getClass().equals(CsvDataset.class)) {
@@ -84,5 +85,4 @@ public class bsModuleImpl implements bsModuleInterface{
         }
         return bootstrapM;
     }
-    */
 }
