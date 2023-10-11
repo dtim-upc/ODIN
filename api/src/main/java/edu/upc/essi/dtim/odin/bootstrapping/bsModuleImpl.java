902c836721275fc1a01c13417444e700e01d3399
Víctor Asenjo <victorasenj@gmail.com>
2023-10-11 10:56:27 +0200

    direct implementation of the bs interface

--- api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleImpl.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/bootstrapping/bsModuleImpl.java
@@ -1,6 +1,6 @@
 package edu.upc.essi.dtim.odin.bootstrapping;
 
-import edu.upc.essi.dtim.NextiaCore.datasources.dataset.Dataset;
+import edu.upc.essi.dtim.NextiaCore.datasources.dataset.*;
 import edu.upc.essi.dtim.NextiaCore.graph.Graph;
 import edu.upc.essi.dtim.nextiabs.*;
 
@@ -17,7 +17,20 @@ public class bsModuleImpl implements bsModuleInterface{
      * @return Un grafo que representa el conjunto de datos.
      */
     public Graph convertDatasetToGraph(Dataset dataset) {
-        NextiaBootstrapInterface bootstrapInterface = new NextiaBootstrapImpl();
+        NextiaBootstrapInterface bootstrapInterface = null;
+
+        if (dataset.getClass().equals(CsvDataset.class)) {
+            bootstrapInterface = new CSVBootstrap_with_DataFrame_MM_without_Jena();
+        } else if (dataset.getClass().equals(JsonDataset.class)) {
+            bootstrapInterface = new JSONBootstrap_with_DataFrame_MM_without_Jena();
+        } else if (dataset.getClass().equals(SQLDataset.class)) {
+            bootstrapInterface = new SQLBootstrap_with_DataFrame_MM_without_Jena();
+        } else if (dataset.getClass().equals(XmlDataset.class)) {
+            bootstrapInterface = new XMLBootstrap_with_DataFrame_MM_without_Jena();
+        } else if (dataset.getClass().equals(ParquetDataset.class)) {
+            bootstrapInterface = new ParquetBootstrap_with_DataFrame_MM_without_Jena();
+        }
+
         return bootstrapInterface.bootstrap(dataset);
     }
 }
