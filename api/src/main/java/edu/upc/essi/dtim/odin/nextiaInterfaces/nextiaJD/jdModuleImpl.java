f7a24652805016dbc113e75876bbb43153bbbdbd
Víctor Asenjo <75260498+victor-asenjo@users.noreply.github.com>
2023-11-07 13:39:27 +0100

    Update jdModuleImpl.java to use DataLayer factory

--- api/src/main/java/edu/upc/essi/dtim/odin/nextiaInterfaces/nextiaJD/jdModuleImpl.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/nextiaInterfaces/nextiaJD/jdModuleImpl.java
@@ -5,6 +5,7 @@ import edu.upc.essi.dtim.NextiaCore.discovery.Alignment;
 import edu.upc.essi.dtim.NextiaDataLayer.materialized.DLMDuckDB;
 import edu.upc.essi.dtim.NextiaDataLayer.materialized.DLMSpark;
 import edu.upc.essi.dtim.NextiaDataLayer.materialized.DataLayerMaterialized;
+import edu.upc.essi.dtim.NextiaDataLayer.utils.DataLayerFactory;
 import edu.upc.essi.dtim.NextiaJD.Discovery;
 import edu.upc.essi.dtim.odin.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,22 @@ import java.util.List;
 public class jdModuleImpl implements jdModuleInterface {
 
     private final String dataLayerPath;
+    private final String technology;
+
     public jdModuleImpl(@Autowired AppConfig appConfig) {
         this.dataLayerPath = appConfig.getDataLayerPath();
+        this.technology = appConfig.getDataLayerTechnology();
     }
 
     @Override
     public List<Alignment> getAlignments(Dataset dataset, Dataset dsB) throws SQLException, IOException, ClassNotFoundException {
-        DataLayerMaterialized dlm = new DLMDuckDB(dataLayerPath);
+        DataLayerMaterialized dlm = null;
+        try {
+            dlm = DataLayerFactory.getInstance(technology, dataLayerPath);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        new DLMDuckDB(dataLayerPath);
         Discovery discovery = new Discovery(dlm);
         try {
             List<Alignment> alignmentsJD = discovery.getAlignments(dataset, dsB);
