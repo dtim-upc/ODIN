63be5cc31227aee9ec98f37e4eef30ad4e3ba13e
Víctor Asenjo <75260498+victor-asenjo@users.noreply.github.com>
2023-11-09 21:14:49 +0100

    wip query infrastucture

--- api/src/main/java/edu/upc/essi/dtim/odin/query/QueryController.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/query/QueryController.java
@@ -22,7 +22,7 @@ public class QueryController {
 
 
     @Autowired
-    private QueryService Iservice;
+    private QueryService queryService;
 
     //    fromGraphicalToSPARQL
     @PostMapping(value="/query/{id}/graphical")
@@ -31,43 +31,9 @@ public class QueryController {
 
         LOGGER.info("[POST /query/fromGraphicalToSPARQL/]");
 
-        // Crear una instancia de RDFSResult
-        RDFSResult res = new RDFSResult();
-
-        // Configurar las columnas
-        res.setColumns(Arrays.asList("Subject", "Predicate", "Object", "DataType"));
-
-        // Configurar algunas filas de datos hardcodeados
-        List<String> row1 = Arrays.asList("Person1", "hasName", "John Doe", "xsd:string");
-
-        // Adaptar las filas al formato necesario
-        List<String> adaptedRows = new ArrayList<>();
-
-        Map<String, String> rowMap = new HashMap<>();
-        for (int i = 0; i < res.getColumns().size(); i++) {
-            rowMap.put(res.getColumns().get(i), row1.get(i));
-        }
-
-        // Convertir el mapa a una cadena JSON
-        String rowJson = convertMapToJsonString(rowMap);
-
-        adaptedRows.add(rowJson);
-
-        // Asignar las filas adaptadas a la instancia de RDFSResult
-        res.setRows(adaptedRows);
+        RDFSResult res = queryService.getQueryResult();
 
         return new ResponseEntity<>(res, HttpStatus.OK);
-    }
-
-    // Método para convertir un mapa a una cadena JSON
-    private String convertMapToJsonString(Map<String, String> map) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.writeValueAsString(map);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace(); // Manejar la excepción según tus necesidades
-            return null;
-        }
     }
 
 }
--- api/src/main/java/edu/upc/essi/dtim/odin/query/QueryService.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/query/QueryService.java
@@ -1,7 +1,62 @@
 package edu.upc.essi.dtim.odin.query;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.upc.essi.dtim.odin.query.pojos.RDFSResult;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class QueryService {
+    public RDFSResult getQueryResult() {
+        // Crear una instancia de RDFSResult
+        RDFSResult res = new RDFSResult();
+
+        // Configurar las columnas
+        res.setColumns(Arrays.asList("Subject", "Predicate", "Object", "DataType"));
+
+        // Filas hardcodeadas
+        List<List<String>> hardCodedRows = Arrays.asList(
+                Arrays.asList("Person1", "hasName", "John Doe", "xsd:string"),
+                Arrays.asList("Person2", "hasAge", "25", "xsd:integer"),
+                Arrays.asList("Person3", "hasOccupation", "Engineer", "xsd:string"),
+                Arrays.asList("Person4", "hasCity", "New York", "xsd:string"),
+                Arrays.asList("Person5", "hasAge", "30", "xsd:integer"),
+                Arrays.asList("Person6", "hasOccupation", "Doctor", "xsd:string"),
+                Arrays.asList("Person7", "hasCity", "San Francisco", "xsd:string"),
+                Arrays.asList("Person8", "hasAge", "28", "xsd:integer"),
+                Arrays.asList("Person9", "hasOccupation", "Teacher", "xsd:string"),
+                Arrays.asList("Person10", "hasCity", "London", "xsd:string")
+        );
+
+        // Adaptar las filas al formato necesario
+        List<String> adaptedRows = new ArrayList<>();
+
+        for (List<String> rowData : hardCodedRows) {
+            Map<String, String> rowMap = new HashMap<>();
+            for (int i = 0; i < res.getColumns().size(); i++) {
+                rowMap.put(res.getColumns().get(i), rowData.get(i));
+            }
+            // Convertir el mapa a una cadena JSON y agregarlo a la lista
+            String rowJson = convertMapToJsonString(rowMap);
+            adaptedRows.add(rowJson);
+        }
+
+        // Asignar las filas adaptadas a la instancia de RDFSResult
+        res.setRows(adaptedRows);
+
+        return res;
+    }
+
+    // Método para convertir un mapa a una cadena JSON
+    private String convertMapToJsonString(Map<String, String> map) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // Manejar la excepción según tus necesidades
+            return null;
+        }
+    }
 }
