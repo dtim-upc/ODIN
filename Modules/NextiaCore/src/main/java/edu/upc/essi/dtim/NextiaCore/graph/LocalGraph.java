9b40f04bcbf4593f616491b9e5ff86ad86d62f77
Marc Maynou <marcmaynou99@gmail.com>
2024-02-20 13:48:27 +0100

    LocalGraph deserialization

--- Modules/NextiaCore/src/main/java/edu/upc/essi/dtim/NextiaCore/graph/LocalGraph.java
+++ Modules/NextiaCore/src/main/java/edu/upc/essi/dtim/NextiaCore/graph/LocalGraph.java
@@ -1,7 +1,20 @@
 package edu.upc.essi.dtim.NextiaCore.graph;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import edu.upc.essi.dtim.NextiaCore.graph.jena.LocalGraphJenaImpl;
+
 import java.util.List;
 import java.util.Map;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = LocalGraphJenaImpl.class, name = "LocalGraphJenaImpl"),
+})
 
 public interface LocalGraph extends Graph {
 
